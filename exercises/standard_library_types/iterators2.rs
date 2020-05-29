e0dd3844fc7f432cc7f2fd763a5d9e055942f09f
Enrico Bozzolini <boz@ti8m.ch>
2020-05-29 22:02:28 +0200

    Solve exercise clippy, arc1 and iterators2

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -7,14 +7,17 @@
 //         Try to ensure it returns a single string.
 // As always, there are hints if you execute `rustlings hint iterators2`!
 
-// I AM NOT DONE
-
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.collect::<String>() + c.as_str(),
+        // TODO: Check why we would use collect() here?
+        Some(first) => first.to_uppercase().to_string() + c.as_str(),
     }
+}
+
+pub fn capitalize_words(input: Vec<&str>) -> Vec<String> {
+    input.iter().map(|word| capitalize_first(word)).collect::<Vec<String>>()
 }
 
 #[cfg(test)]
@@ -26,6 +29,7 @@ mod tests {
     #[test]
     fn test_success() {
         assert_eq!(capitalize_first("hello"), "Hello");
+        assert_eq!(capitalize_first("é"), "É");
     }
 
     #[test]
@@ -37,14 +41,14 @@ mod tests {
     #[test]
     fn test_iterate_string_vec() {
         let words = vec!["hello", "world"];
-        let capitalized_words: Vec<String> = // TODO
+        let capitalized_words: Vec<String> = capitalize_words(words);
         assert_eq!(capitalized_words, ["Hello", "World"]);
     }
 
     #[test]
     fn test_iterate_into_string() {
         let words = vec!["hello", " ", "world"];
-        let capitalized_words = // TODO
+        let capitalized_words = capitalize_words(words).join("");
         assert_eq!(capitalized_words, "Hello World");
     }
 }
